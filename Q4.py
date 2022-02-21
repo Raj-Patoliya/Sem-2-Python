d2c603b201e4b947e6bc2d7479535e7d5cf8fff3
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2022-02-21 13:14:46 +0530

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,22 @@
+def add(a,b):
+	return a+b
+def sub(a,b):
+	return a-b
+def mul(a,b):
+	return a*b
+def div(a,b):
+	return a/b
+
+
+
+print("\n\t1 For Addition")
+print("\n\t2 For Substratcion ")
+print("\n\t3 For Multiplication")
+print("\n\t4 For Division")
+print("\n\t0 For Exit")
+ch = input("\n\tSelect your Choice : ")	
+
+  if ch = 1:  
+	a = input("\n\t A = ")
+	b = input("\n\t B = ")
+	add(a,b)
