c163165db5b5efe643b415b7789a4e1d6cb6f460
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2022-02-21 12:33:30 +0530

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,8 @@
+# Write a program in python to swap two variables without using temporary variable
+
+dec = int(input("\n\t Enter The Decimal Value : "))
+	
+print("\n\tThe decimal value is "+ str(dec))
+print("\n\t"+str(dec)+" in binary " +bin(dec))
+print("\n\t"+str(dec)+" in binary " +oct(dec))
+print("\n\t"+str(dec)+" in hexadecimal " +hex(dec))
