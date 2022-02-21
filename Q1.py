d826465f5083081955f9aa1a1083223c459398e4
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2022-02-21 11:53:36 +0530

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,19 @@
+def fah(fahrenheit):
+	celsius = ( fahrenheit - 32) * 5.0/9.0
+	print("\n\tFahrenheit "+ str(fahrenheit))
+	print("\n\tcelsius "+ str(celsius))
+
+
+def cel(celsius):
+	fahrenheit = 9.0/5.0 * celsius + 32
+	print(" \n\tcelsius "+ str(celsius))
+	print(" \n\tFahrenheit "+ str(fahrenheit))
+
+raj = input("\n\tInput Temprature :  ")
+print("\n********************************************************************")
+print("\n\tfahrenheit To Celsius ")
+fah(float(raj))
+print("\n********************************************************************")
+print("\n\tCelsius To fahrenheit ")
+cel(float(raj))
+print("\n********************************************************************")
