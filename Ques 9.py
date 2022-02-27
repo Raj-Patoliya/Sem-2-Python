352bc0a54e90967ea9174b7e7aba88489c5f68c6
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2022-02-27 08:37:42 +0530

    Create Ques 9.py

---
+++ Ques 9.py
@@ -0,0 +1,14 @@
+n = int(input("\n\tEnter Number : "))
+n1 = 0
+n2 = 1
+i=0
+print(" "+str(n1))
+print(" "+str(n2))
+int(n1)
+int(n2)
+while(i!=n):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    i=i+1
+    print(" "+str(n3))
