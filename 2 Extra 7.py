00332db211bf855cd1a4cc49babd9beb4f6f3842
Raj Patoliya <84698326+Raj-Patoliya@users.noreply.github.com>
2022-03-14 13:17:20 +0530

    Update 2 Extra 7.py

--- 2 Extra 7.py
+++ 2 Extra 7.py
@@ -1,7 +1,4 @@
-def part(lust):
-	eve =[]
-	odd = []	
-	list(filter(lambda x:eve.append(x) if  x%2 == 0 else odd.append(x),lust))
-	print(eve)
-	print(odd)
-part(lust = [ int(x) for x in input().split(",") ])
+eve =[]
+odd = []	
+list(filter(lambda x:eve.append(x) if  x%2 == 0 else odd.append(x),(int(x) for x in input("\n\t").split(","))))
+print("\n\t",eve,"\n\t",odd)
